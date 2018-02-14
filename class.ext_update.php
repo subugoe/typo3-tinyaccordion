8fa449566105192a0821b25d6d01abd4c48f24d5
Ingo Pfennigstorf <i.pfennigstorf@gmail.com>
2018-02-14 11:28:10 +0100

    Fix update script

--- class.ext_update.php
+++ class.ext_update.php
@@ -16,17 +16,9 @@ class ext_update
      */
     protected $messageArray = [];
 
-    /**
-     * @var \TYPO3\CMS\Core\Database\DatabaseConnection
-     */
-    protected $databaseConnection;
-
-    /**
-     * Constructor
-     */
-    public function __construct()
+    private function getDatabaseConnection(): \TYPO3\CMS\Core\Database\DatabaseConnection
     {
-        $this->databaseConnection = $GLOBALS['TYPO3_DB'];
+        return $GLOBALS['TYPO3_DB'];
     }
 
     /**
@@ -57,7 +49,7 @@ class ext_update
     {
         $title = 'Update tt_content (plug-in list type).';
         $update = ['list_type' => 'tinyaccordion_pi1'];
-        $this->databaseConnection->exec_UPDATEquery(
+        $this->getDatabaseConnection()->exec_UPDATEquery(
             'tt_content',
             "(list_type='tinyaccordion_tinyaccordion' OR list_type='tinyaccordion_Pi1')",
             $update
@@ -65,19 +57,19 @@ class ext_update
         $this->messageArray[] = [FlashMessage::OK, $title, 'tt_content has been updated!'];
 
         $title = 'Update sys_template (path to the TypoScript files).';
-        $GLOBALS['TYPO3_DB']->sql_query(
+        $this->getDatabaseConnection()->sql_query(
             "UPDATE sys_template SET include_static_file = replace(include_static_file, 'tinyaccordion/Configuration/TypoScript7', 'tinyaccordion/Configuration/TypoScript') WHERE include_static_file!=''"
         );
         $this->messageArray[] = [FlashMessage::OK, $title, 'sys_template has been updated!'];
 
         $title = 'Update sys_template (path to the HTML templates).';
-        $GLOBALS['TYPO3_DB']->sql_query(
+        $this->getDatabaseConnection()->sql_query(
             "UPDATE sys_template SET config = replace(config, 'plugin.tx_tinyaccordion.view.partialRootPath =', 'plugin.tx_tinyaccordion.view.partialRootPaths.1 =') WHERE 1=1"
         );
-        $GLOBALS['TYPO3_DB']->sql_query(
+        $this->getDatabaseConnection()->sql_query(
             "UPDATE sys_template SET config = replace(config, 'plugin.tx_tinyaccordion.view.templateRootPath =', 'plugin.tx_tinyaccordion.view.templateRootPaths.1 =') WHERE 1=1"
         );
-        $GLOBALS['TYPO3_DB']->sql_query(
+        $this->getDatabaseConnection()->sql_query(
             "UPDATE sys_template SET config = replace(config, 'plugin.tx_tinyaccordion.view.layoutRootPath =', 'plugin.tx_tinyaccordion.view.layoutRootPaths.1 =') WHERE 1=1"
         );
         $this->messageArray[] = [FlashMessage::OK, $title, 'sys_template has been updated!'];
@@ -90,17 +82,20 @@ class ext_update
      */
     protected function generateOutput()
     {
-        $output = '';
+        $out = GeneralUtility::makeInstance(\TYPO3\CMS\Core\Messaging\Renderer\BootstrapRenderer::class);
+
+        $messages = [];
+
         foreach ($this->messageArray as $messageItem) {
             /** @var \TYPO3\CMS\Core\Messaging\FlashMessage $flashMessage */
             $flashMessage = GeneralUtility::makeInstance(
-                'TYPO3\\CMS\\Core\\Messaging\\FlashMessage',
+                FlashMessage::class,
                 $messageItem[2],
                 $messageItem[1],
                 $messageItem[0]
             );
-            $output .= $flashMessage->render();
+            $messages[] = $flashMessage;
         }
-        return $output;
+        return $out->render($messages);
     }
 }
